cceb86eb72a2a95bfa112da232f56a85e7982aa7
bint-Eve <khanrukhsar1890@gmail.com>
2025-01-03 23:51:33 +0530

    switchmap-ng-web [ navbar/Css changes ] (#10)
    * Update pull-request.yml
    
    * Merge from the `develop` branch (#9)
    
    * Minor updates
    
    * Removed blog
    
    * Docusaurus URL update
    
    * Updated sensitive files
    
    * Reverted sensitive files
    
    * Update push-deploy-website.yml
    
    * Update pull-request.yml
    
    * Update README.md
    
    * Update README.md
    
    * Test
    
    * Added markdown files
    
    * Updated markdown (#8)
    
    * switchmap-ng-web initial
    
    * switchmap-ng-web initial
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Peter Harrison <16875803+palisadoes@users.noreply.github.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,119 +1,176 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Switchmap-NG Web Documentation',
-  tagline: 'Start your open source journey here',
-  favicon: 'img/favicon.ico',
+  title: "Switchmap-NG Web Documentation",
+  tagline: "Start building your Switchmap-NG project today",
+  favicon: "img/icons/favicon_palisadoes.ico",
+  
+  url: "https://docs-admin.switchmap-ng.io",
+  baseUrl: "/",
+  deploymentBranch: "gh-pages",
+  
+  organizationName: "PalisadoesFoundation", // GitHub org
+  projectName: "switchmap-ng-web", // repo name
 
-  url: 'https://docs-admin.switchmap-ng.io',
-  baseUrl: '/',
-  deploymentBranch: 'gh-pages',
-
-  organizationName: 'PalisadoesFoundation', // GitHub org
-  projectName: 'switchmap-ng-web', // repo name
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    {
+      docs: {
+        sidebar: {
+          hideable: false,
+        },
       },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+      navbar: {
+        title: "Switchmap-NG Web Documentation",
+        logo: {
+          alt: "Talawa Logo",
+          src: "img/icons/favicon_palisadoes.ico",
+          className: "LogoAnimation",
         },
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            label: "General",
+            position: "left",
+          },
+          {
+            label: "Web Guide",
+            position: "left",
+            href: "https://docs-web.switchmap-ng.io/",
+            target: "_self",
+          },
+          {
+            label: "Poller Guide",
+            position: "left",
+            href: "https://docs-poller.switchmap-ng.io/",
+            target: "_self",
+          },
+          {
+            label: "API Guide",
+            position: "left",
+            href: "https://docs-api.switchmap-ng.io/",
+            target: "_self",
+          },
+
+          {
+            label: "Demo",
+            position: "left",
+            href: "http://admin-demo.talawa.io/",
+          },
+          {
+            to: "https://github.com/PalisadoesFoundation",
+            position: "right",
+            className: "header-github-link",
+            "aria-label": "GitHub repository",
+          },
+          {
+            to: "https://www.youtube.com/@PalisadoesOrganization",
+            position: "right",
+            className: "header-youtube-link",
+            "aria-label": "Palisadoes Youtube channel",
+          },
+        ],
+      },
+      colorMode: {
+        defaultMode: "light",
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Community",
+            items: [
+              {
+                label: " Slack",
+                to: "https://github.com/PalisadoesFoundation",
+                className: "footer__icon footer__slack",
+              },
+              {
+                label: " News",
+                to: "https://www.palisadoes.org/news/",
+                className: "footer__icon footer__news",
+              },
+              {
+                label: " Contact Us",
+                to: "https://www.palisadoes.org/contact/",
+                className: "footer__icon footer__contact",
+              },
+            ],
+          },
+          {
+            title: "Social Media",
+            items: [
+              {
+                label: " Twitter",
+                to: "https://twitter.com/palisadoesorg?lang=en",
+                className: "footer__icon footer__twitter",
+              },
+              {
+                label: " Facebook",
+                to: "https://www.facebook.com/palisadoesproject/",
+                className: "footer__icon footer__facebook",
+              },
+              {
+                label: " Instagram",
+                to: "https://www.instagram.com/palisadoes/?hl=en",
+                className: "footer__icon footer__instagram",
+              },
+            ],
+          },
+          {
+            title: "Development",
+            items: [
+              {
+                label: " GitHub",
+                to: "https://github.com/PalisadoesFoundation",
+                className: "footer__icon footer__github",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} The Palisadoes Foundation, LLC. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
 };
 
 export default config;
